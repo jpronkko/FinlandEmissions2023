e78f490be92e256771971b56c039df16c52536e7
jpronkko <jukka.ronkko@iki.fi>
2024-09-05 18:06:22 +0300

    Python cleanups, command line tool to accept output filename as argument

--- Python/retrive_gas_emissions_statfi.py
+++ Python/retrive_gas_emissions_statfi.py
@@ -1,8 +1,13 @@
-# Simply downloads data from the StatFin public API and dumps it to the terminal.
-# Pipe the dump to a file and go from there.
+# Simply downloads data from the StatFin public API and dumps it to file given as argument.
 
 import requests
+import sys
 
+if len(sys.argv) != 2:
+  print(f"Usage: python ${sys.argv[0]} <output_file>")
+  sys.exit(1)
+
+output_file = sys.argv[1]
 url = 'https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/khki/statfin_khki_pxt_138v.px'
 
 query = {
@@ -25,10 +30,14 @@ query = {
     }
   ],
   "response": {
-    "format": "json-stat2"
+    "format": "csv"
   }
 }
 
 response = requests.post(url, json=query)
-data = response.json()
-print(data)
+data = response.text
+
+with open(output_file, 'w') as f:
+  f.write(data)
+
+print(f"Data written to {output_file}")
